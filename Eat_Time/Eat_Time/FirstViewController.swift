c2a1ee8d79acc2415c192c0e98c6945c1543cf21
Simin <sliu19@illinois.edu>
2015-09-08 18:29:54 -0700

    Initial Commit

---
+++ Eat_Time/Eat_Time/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Eat_Time
+//
+//  Created by Simin Liu on 9/8/15.
+//  Copyright (c) 2015 Simin Liu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
