957295a5a0e358b31f84892d4b702dfe6626bd39
Simin <sliu19@illinois.edu>
2015-09-14 00:14:24 -0700

    connet facebook && Parse SDK

---
+++ Eat_Time/Eat_Time/MealTabView/NewEventViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewEventViewController.swift
+//  Eat_Time
+//
+//  Created by Simin Liu on 9/13/15.
+//  Copyright (c) 2015 Simin Liu. All rights reserved.
+//
+
+import Cocoa
+
+class NewEventViewController: UIViewController {
+
+}
---
+++ Eat_Time/Eat_Time/util/objc-link.m
@@ -0,0 +1,9 @@
+//
+//  objc-link.m
+//  Eat_Time
+//
+//  Created by Simin Liu on 9/13/15.
+//  Copyright (c) 2015 Simin Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
